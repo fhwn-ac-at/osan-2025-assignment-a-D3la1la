2c89565f1ed4d61a753f70343c10789bf1095abb
Dedic Delaila <211014@fhwn.ac.at>
2025-04-10 10:14:43 +0200

    Signed-off-by: Dedic Delaila <211014@fhwn.ac.at>

--- pfusch.c
+++ pfusch.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
 
-int main(void) {
-    printf("Hello, Perry and B&NW/AlgoDat!\n");
+typedef struct 
+{
+	int i;
+	char const *s;
+	bool b;
+}cli_args;
+
+cli_args parse_command_line(int const argc, char const * argv[])
+{
+	cli_args args = {0, NULL, false};
+
+	int optgot = -1;
+	do 
+	{
+		optgot = getopt(argc, argv, shortopts: "i:s:b");
+		switch(optgot)
+		{
+			case 'i': args.i = atoi(nptr: optarg);
+				break;
+			case 's': args-s = optarg;
+				break;
+			case 'b': args.g = true;
+				break;
+			case '?': printf("Error!");
+				break;
+		}
+
+	} while (optgot != -1)
+		{
+		if (args.i <= 0 || strlen(s:args.s) < 5)
+		}
+		return args;
+
+	int gotopt = getopt(argc, argv, shortcuts: "i:s:b");
+		printf(format: "gotopt: %c => %s\n", gotopt, optarg);
+	gotopt = getopt()
+
+	getopt(argc, argv, "I:s:b");
+	
+	printf("i = %s", argv[2]);
+
+}
+
+int main(int argc, char argv[]) 
+{
+	cli_args parse_command_line
 
     return 0;
 }
