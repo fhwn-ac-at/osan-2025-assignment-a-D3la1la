e2e06309c7f6522d690ca4a8141bcdfad5d6d88d
Delaila Dedic <211014@fhwn.ac.at>
2025-04-16 18:29:04 +0200

    Signed-off-by: Delaila Dedic <211014@fhwn.ac.at>

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/H1vNwaly)
 # Protokoll 
-
-Hello, World!
+*Hello_World!
 
 
 
@@ -10,23 +9,40 @@ Protokoll 3
 Thema: Systemprogrammierung
 Name: Delaila Dedic - 211014
 
-Gelernte Befehle/Anwendungen im Unterricht:
-Das es das man(manual) in Ubuntu gibt
+#Gelernte Befehle/Anwendungen im Unterricht:
+Das es das man(manual) in Ubuntu gibt und wie man es öffnet bzw. bedient. 
 Befehle wie pid_t, mq_[jegliche Unterbefehle], mq_t, EXIT_SUCCESSFUL, getopt ect. erstes Mal gesehen und kennengelernt. 
 bool Befehle erstes Mal auch in Anwendungen gesehen, kannte man vom Namen her bei anderen LVs aber schon etwas
 Viele neue Header kennengelernt (unistd.h, fcntl.h, mqueue.h, sys/wait.h, sys/types.h,...)
 
-Generell etwas mehr zur Visual Studio Codierung (Shortcuts, ect.). 
+Generell etwas mehr zur Visual Studio Codierung (Shortcuts, ect.) gelernt. 
+Und vieles weiteres das in der Coding Geschwindigkeit im Unterricht_Code_Parts.c File zum Teil vorhanden ist.
 
 
-Zusätzlich genutzte Informationen um Aufgabenstellung zu bewältigen:
+
+#Aufgabe Ventilator und Worker Dateien
+ventilator.c ist die C File die als Hauptcode mit der int main() Funktion sich jene Befehle von worker.c holt und bearbeitet.
+	worker.c währendessen behandelt wie der Name schon sagt das Aufteilen der "Arbeiter" an Aufgaben 
+	bzw. die genauen Auführungen werden dann in der Command Line mit 
+	zum Beispiel (wenn alles am Ende wirklich funktioniert) mit "$ ./ventilator -w 4 -t 10 -s 2" ausgeführt. 
+																	w=Arbeiter, t=Aufgabenanzahl, s=Bearbeitungsdauer/Aufwand/...
+
+Generell hat man den Code am Anfang unabhängig bzw. in 1 C File gehabt um überhaupt zu compilen
+ohne out und executable zu erstellen um beide gleichzeitig auszuführen.
+
+
+Sonst lässt sich noch kurz sagen das man einiges an neuen Befehlen/Headern/Arbeitsweisen/... anwenden/probieren/versuchen konnte.
+Die Aufgabe auch im Vergleich zu vorherigen Aufgaben eine viel komplexere/verschachtelte/aufwändigere war.		
+
+#Zusätzlich genutzte Informationen um Aufgabenstellung zu bewältigen:
 
 Vorher noch nie mit Makefile gearbeitet in irgendeiner Hinsicht. Alles was mit Makefile zu tun hatte war neu. Daher 
 kann es nach vielen Versuchen und dem zusätzlichen Einschalten von ChatGPT für den Code 
-trotzdem noch sein das es nicht richtig compiliert. 
-
+trotzdem noch sein das es nicht richtig compiliert bzw. bei Files miteinander vollständig arbeiten.
+ 
 Generell auch KI für genauere Erläuterungen und bessere Code Struktur/Header file Aufteillung aber auch bei den 
-mq, ect. Befehlen/Korrekturen gefragt. Sowie die Header files generell auch mit man und/oder mit KI versucht zu verstehen und zu unterscheiden. 
+mq, ect. Befehlen/Korrekturen/Umschreiben gefragt. Sowie die Header files generell auch mit man und/oder mit KI versucht zu verstehen und zu unterscheiden. 
+
 
 
 
--- ventilator.c
+++ ventilator.c
@@ -9,93 +9,130 @@
 #include <string.h>
 #include <errno.h>
 
+// Define queue names
 #define TASK_QUEUE "/task_queue"
 #define RESULT_QUEUE "/result_queue"
 
-typedef struct {
-    int effort;
+// Define the structure of a Task
+typedef struct
+{
+    int effort; // Represents the effort required for a task
 } Task;
 
-typedef struct {
-    int worker_id;
-    pid_t pid;
-    int task_count;
-    int total_time;
+// Define the structure for worker results
+typedef struct
+{
+    int worker_id;    // Worker ID
+    pid_t pid;        // Process ID of the worker
+    int task_count;   // Number of tasks the worker processed
+    int total_time;   // Total time taken by the worker to process tasks
 } Result;
 
+// Function to spawn worker processes
 void spawn_workers(int n);
 
 int main(int argc, char *argv[])
 {
-    if (argc != 7) {
+    // Check if correct number of arguments is passed
+    if (argc != 7) 
+    {
         fprintf(stderr, "Usage: %s -w <workers> -t <tasks> -s <queue_size>\n", argv[0]);
         exit(1);
     }
 
-    int workers = atoi(argv[2]);
-    int tasks = atoi(argv[4]);
-    int queue_size = atoi(argv[6]);
+    // Parse command-line arguments
+    int workers = atoi(argv[2]);    // Number of worker processes
+    int tasks = atoi(argv[4]);      // Number of tasks to distribute
+    int queue_size = atoi(argv[6]); // Maximum number of tasks in the queue
 
-    printf("%s | Ventilator | Starting %d workers for %d tasks and a queue size of %d\n", __TIME__, workers, tasks, queue_size);
+    // Log the start of the program
+    printf("%s | Ventilator | Starting %d workers for %d tasks and a queue size of %d\n", 
+           __TIME__, workers, tasks, queue_size);
 
-    struct mq_attr attr = {0};
-    attr.mq_maxmsg = queue_size;
-    attr.mq_msgsize = sizeof(Task);
+    // Set up message queue attributes
+    struct mq_attr attr = { 0 };
+    attr.mq_maxmsg = queue_size;  // Max number of messages in the queue
+    attr.mq_msgsize = sizeof(Task); // Message size for each task
 
+    // Clean up any pre-existing message queues
     mq_unlink(TASK_QUEUE);
     mq_unlink(RESULT_QUEUE);
 
+    // Open the message queues (task queue for writing, result queue for reading)
     mqd_t task_q = mq_open(TASK_QUEUE, O_CREAT | O_WRONLY, 0644, &attr);
     mqd_t result_q = mq_open(RESULT_QUEUE, O_CREAT | O_RDONLY, 0644, NULL);
 
-    if (task_q == -1 || result_q == -1) {
+    // Check for errors in opening the message queues
+    if (task_q == -1 || result_q == -1) 
+    {
         perror("mq_open");
         exit(1);
     }
 
-    for (int i = 0; i < workers; i++) {
+    // Fork worker processes
+    for (int i = 0; i < workers; i++) 
+    {
         pid_t pid = fork();
-        if (pid == 0) {
+        
+        if (pid == 0) 
+        {
+            // Child process (worker)
             char id_str[10];
             sprintf(id_str, "%d", i + 1);
-            execl("./worker", "worker", id_str, NULL);  // Now calling the worker process
-            perror("execl");
+            execl("./worker", "worker", id_str, NULL); // Execute the worker process
+            perror("execl");  // If execl fails
             exit(1);
-        } else {
+        } 
+        else 
+        {
+            // Parent process (Ventilator)
             printf("%s | Worker #%02d | Started worker PID %d\n", __TIME__, i + 1, pid);
         }
     }
 
+    // Seed the random number generator and start task distribution
     srand(time(NULL));
     printf("%s | Ventilator | Distributing tasks\n", __TIME__);
 
-    for (int i = 0; i < tasks; i++) {
-        Task t = { .effort = (rand() % 10) + 1 };
-        printf("%s | Ventilator | Queuing task #%d with effort %d\n", __TIME__, i+1, t.effort);
-        mq_send(task_q, (char*)&t, sizeof(Task), 0);
+    // Distribute tasks to the task queue
+    for (int i = 0; i < tasks; i++) 
+    {
+        Task t = { .effort = (rand() % 10) + 1 }; // Random effort between 1 and 10
+        printf("%s | Ventilator | Queuing task #%d with effort %d\n", __TIME__, i + 1, t.effort);
+        mq_send(task_q, (char*)&t, sizeof(Task), 0); // Send task to the queue
     }
 
+    // Send termination task to all workers
     printf("%s | Ventilator | Sending termination tasks\n", __TIME__);
-    Task term = { .effort = 0 };
-    for (int i = 0; i < workers; i++) {
-        mq_send(task_q, (char*)&term, sizeof(Task), 0);
+    Task term = { .effort = 0 }; // Termination task (effort 0)
+    for (int i = 0; i < workers; i++) 
+    {
+        mq_send(task_q, (char*)&term, sizeof(Task), 0); // Send termination signal to each worker
     }
 
-    for (int i = 0; i < workers; i++) {
+    // Receive results from workers
+    for (int i = 0; i < workers; i++) 
+    {
         Result r;
-        mq_receive(result_q, (char*)&r, sizeof(Result), NULL);
-        printf("%s | Ventilator | Worker %d processed %d tasks in %d seconds\n", __TIME__, r.worker_id, r.task_count, r.total_time);
+        mq_receive(result_q, (char*)&r, sizeof(Result), NULL); // Receive worker result
+        printf("%s | Ventilator | Worker %d processed %d tasks in %d seconds\n", 
+               __TIME__, r.worker_id, r.task_count, r.total_time);
     }
 
-    for (int i = 0; i < workers; i++) {
+    // Wait for worker processes to finish
+    for (int i = 0; i < workers; i++) 
+    {
         int status;
-        pid_t pid = wait(&status);
-        printf("%s | Ventilator | Worker with PID %d exited with status %d\n", __TIME__, pid, WEXITSTATUS(status));
+        pid_t pid = wait(&status); // Wait for worker to exit
+        printf("%s | Ventilator | Worker with PID %d exited with status %d\n", 
+               __TIME__, pid, WEXITSTATUS(status));
     }
 
+    // Clean up message queues
     mq_close(task_q);
     mq_close(result_q);
     mq_unlink(TASK_QUEUE);
     mq_unlink(RESULT_QUEUE);
+    
     return 0;
 }
